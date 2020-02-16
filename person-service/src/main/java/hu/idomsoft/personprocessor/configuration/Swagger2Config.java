0e9f77c1a6510fcc28707e3c07f568d48b6aae0a
Gábor Zajti <zajtig@gmail.com>
2020-02-16 09:27:10 +0100

    Sonar

--- person-service/src/main/java/hu/idomsoft/personprocessor/configuration/Swagger2Config.java
+++ person-service/src/main/java/hu/idomsoft/personprocessor/configuration/Swagger2Config.java
@@ -1,7 +1,6 @@
 package hu.idomsoft.personprocessor.configuration;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -10,11 +9,7 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.UiConfiguration;
-import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
